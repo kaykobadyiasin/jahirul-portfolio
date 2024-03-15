9f3e7a69ebc6a1a0589d2cfcaa6e01f73a36e8c0
Kaykobad Ahammed Yiasin <kaykobadyiasin@gmail.com>
2024-03-16 00:41:37 +0600

    started portfolio work

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-bold bg-blue-800 text-primaryColor-100">Portfolio</h2>
+        </div>
+    );
+};
+
+export default Home;
